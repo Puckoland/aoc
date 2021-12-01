a5634701d53f425df5f5ad6f3b9c3f5f0b977620
Filip Karnis <56135420+Puckoland@users.noreply.github.com>
2021-12-01 23:01:17 +0100

    count increments

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,4 +1,10 @@
 lines = open('input.txt', 'r').read().splitlines()
 
+increments = -1
+last = 0
 for line in lines:
-    print(line)
+    if line > last:
+        last = line
+        increments += 1
+
+print(increments)
