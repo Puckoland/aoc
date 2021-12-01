f0cd50c01b2484bf296e10c19aeb68b56f9c3436
Filip Karnis <56135420+Puckoland@users.noreply.github.com>
2021-12-01 23:18:39 +0100

    part2

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,11 +1,34 @@
 lines = open('input.txt', 'r').read().splitlines()
 lines = list(map(lambda x: int(x), lines))
 
-increments = 0
-last = lines[0]
-for line in lines:
-    if line > last:
-        increments += 1
-    last = line
 
-print(increments)
+def part1():
+    increments = 0
+    last = lines[0]
+    for line in lines:
+        if line > last:
+            increments += 1
+        last = line
+    print(increments)
+
+
+def part2():
+    increments = -1
+    last = 0
+    curr = 0
+    window = [0, 0, 0]
+    for line in lines:
+        curr -= window[0]
+        window[0] = window[1]
+        window[1] = window[2]
+        window[2] = line
+        curr += line
+        
+        if curr > last:
+            increments += 1
+        last = curr
+    print(increments)
+
+
+part1()
+part2()
