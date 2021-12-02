565bb607b1d96bd1d03489a43757f8b786d0068a
Filip Karnis <56135420+Puckoland@users.noreply.github.com>
2021-12-02 15:04:44 +0100

    part1

---
+++ 2021/day02.py
@@ -0,0 +1,23 @@
+FORWARD = "forward"
+DOWN = "down"
+UP = "up"
+
+lines = open('input.txt', 'r').read().splitlines()
+
+
+def part1():
+    x = 0
+    depth = 0
+    for line in lines:
+        line.split(' ')
+        command, int(num) = line
+        if command == FORWARD:
+            x += num
+        elif command == UP:
+            depth -= num
+        elif command == DOWN:
+            depth += num
+    print(x * depth)
+
+
+part1()
