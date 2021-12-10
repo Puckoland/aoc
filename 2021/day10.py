14a7cd2883585468abab89c3966a06b4bf6c325f
Filip Karnis <karnisfilip@gmail.com>
2021-12-10 18:47:21 +0100

    part2

--- 2021/day10.py
+++ 2021/day10.py
@@ -2,6 +2,7 @@ lines = open('input.txt').read().splitlines()
 
 parentheses_pair = {')': '(', ']': '[', '}': '{', '>': '<'}
 parentheses_points = {')': 3, ']': 57, '}': 1197, '>': 25137}
+parentheses_points_autocomplete = {'(': 1, '[': 2, '{': 3, '<': 4}
 
 
 def part1():
@@ -20,7 +21,29 @@ def part1():
 
 
 def part2():
-    pass
+    scores = []
+    for line in lines:
+        corrupted = False
+        stack = []
+        for c in line:
+            if c not in parentheses_pair.keys():
+                stack.append(c)
+            else:
+                if not stack:
+                    continue
+                if parentheses_pair[c] != stack.pop():
+                    corrupted = True
+                    break
+        if corrupted:
+            continue
+        points = 0
+        while stack:
+            c = stack.pop()
+            points *= 5
+            points += parentheses_points_autocomplete[c]
+        scores.append(points)
+    scores.sort()
+    print(scores[len(scores) // 2])
 
 
 part1()
