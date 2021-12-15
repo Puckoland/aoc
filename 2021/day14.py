436451576226adc02552febb429f5e8a03880f4f
Filip Karnis <karnisfilip@gmail.com>
2021-12-15 20:03:50 +0100

    part2

--- 2021/day14.py
+++ 2021/day14.py
@@ -1,32 +1,55 @@
-import math
-
 lines = open('input.txt').read().splitlines()
-start = [char for char in lines[0]]
-insert = dict(map(lambda x: x.split(" -> "), lines[2:]))
+start = lines[0]
+rules = dict(map(lambda x: x.split(" -> "), lines[2:]))
 
 
+def change_rules():
+    for key in rules:
+        rules[key] = key[0] + rules[key], rules[key] + key[1]
 
+
+def reduce_rules():
+    for key in rules:
+        fi = key[0] + rules[key]
+        se = rules[key] + key[1]
+        rules[key] = key[0] + rules[fi] + rules[key] + rules[se]
 
 
 def get_result(template):
-    # counts = {}
-    # for char in template:
-    #     if counts.get(char):
-    #         counts[char] += 1
-    #     else:
-    #         counts[char] = 1
-    # return max(counts.values()) - min(counts.values())
-    return template.count(max(template, key=template.count)) - template.count(min(template, key=template.count))
+    # First letter
+    counts = {start[0]: 1}
+    for key in template:
+        se = key[1]
+        if se in counts:
+            counts[se] += template[key]
+        else:
+            counts[se] = template[key]
+    return counts[max(counts, key=counts.get)] - counts[min(counts, key=counts.get)]
 
 
 def do(steps):
-    template = start.copy()
+    template = {}
+    for i in range(len(start) - 1):
+        pair = start[i] + start[i + 1]
+        if pair in template:
+            template[pair] += 1
+        else:
+            template[pair] = 1
     for _ in range(steps):
-        to_add = []
-        for i in range(len(template) - 1):
-            to_add.append(insert.get(template[i] + template[i + 1]))
-        for i, add in enumerate(to_add):
-            template.insert(2 * i + 1, add)
+        new_tem = {}
+        for pair in template:
+            count = template[pair]
+            fi = rules[pair][0]
+            se = rules[pair][1]
+            if fi in new_tem:
+                new_tem[fi] += count
+            else:
+                new_tem[fi] = count
+            if se in new_tem:
+                new_tem[se] += count
+            else:
+                new_tem[se] = count
+        template = new_tem
     print(get_result(template))
 
 
@@ -38,5 +61,6 @@ def part2():
     do(40)
 
 
+change_rules()
 part1()
 part2()
